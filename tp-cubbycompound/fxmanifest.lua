18c21279385aa6a9d8926d3153fb367662f528ea
toxepls <123585534+toxepls@users.noreply.github.com>
2023-05-09 09:45:50 -0400

    Add files via upload

---
+++ tp-cubbycompound/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author 'toxepls' 
+description 'Adds defensive features and gates to Little Seoul cubby' 
+version '1.5.0'
+
+
+this_is_a_map 'yes'
+
+files {
+    'stream/*.ymap*'
+}
+
+file 'stream/**.ymap'
